e1109dd5493614aea23666fd92b34ec079b5d83d
omerfdev <omerfdev@gmail.com>
2023-11-04 17:01:13 +0300

    Add project files.

---
+++ BasicSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BasicSignalR.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string username,string message)
+        {
+           await Clients.All.SendAsync("NewMessage",username, message);
+        }
+        public async Task SendLocation(string color,int x,int y)
+        {
+            await Clients.All.SendAsync("GetLocation",Context.ConnectionId,color,x,y);
+        }
+    }
+}   
